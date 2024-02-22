da46fa1fb3cdd8e587dfbc4e4a2a4a991d57131c
bajadevmx <91626603+bajadevmx@users.noreply.github.com>
2024-02-21 22:30:46 -0800

    Update

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@
 // let strAddress
 // let strCity
 // let strState
-// let intZipCode
+ //let intZipCode
 // let intYourAge
 // let strReferralSource
 // let blnMayWeContactYou
@@ -38,9 +38,87 @@
 
 // let strMyName= "Tania Mendez", blnMayWeContactYou=true
 
-// intZipCode= 92154
+ //intZipCode= 92154
 // blnMayWeContactYou= true
 
 // //STEP 6
 let MyVariable=  "My number is" + 43
 console.log(MyVariable)
+//I didn't find a way to run in the console, it says I need to Install Additional Debuggers, I google it 
+//but I couldn't fix it.
+
+//STEP 7 
+let var1
+let var2
+let blnMayWeContactYou=false
+let intZipCode= 92154
+var1= blnMayWeContactYou + 'Test text'
+var2=  intZipCode + blnMayWeContactYou
+console.log(var2)
+
+//STEP 8
+let someString= 'Who once said,\'Only two things are infinite, the universe and human stupidity, and Im not sure about the former.\''
+console.log(someString)
+
+//STEP 9
+let Var3
+console.log(Var3)
+var3="Variable "
+let numA= 100
+console.log(numA + Var3)
+
+//STEP 11
+let myGreeting= "Hello " 
+let myName= "Tania Mendez, "
+let Phrase= "Welcome to the JavaScript class!"
+ alert(myGreeting + myName + Phrase)
+
+//STEP 12
+let name= "Tania Mendez"
+alert(`Hello  ${name} `)
+
+//STEP 13
+let course= "JavaScript"
+alert(`${myGreeting}${myName} Welcome to the ${course} class!`)
+
+//STEP 14
+alert(`${myGreeting}${myName} \n Welcome to the ${course} class!`)
+
+//STEP 15
+let name= prompt("What's your name?")
+alert(`${myGreeting}${name} Welcome to the ${course} class!`)
+
+//STEP 16
+let course= prompt("What's the class you are taking?")
+alert(`${myGreeting}${name} Welcome to the ${course} class!`)
+
+//STEP 17
+let x= 10
+let y =20
+console.log(x+y)
+
+//STEP 18
+let x=20
+x += 20
+console.log(x)
+
+//STEP 19
+let x=20
+x *=5
+console.log(x)
+
+//STEP 20
+let x=20/3
+x /= 1
+
+//STEP 21
+let a= 12 +8
+let b=16 +4
+console.log(a == b)
+console.log(a+b)
+
+//STEP 22
+let a= 9
+let b=19
+console.log(a < b)
+console.log(a+b)
