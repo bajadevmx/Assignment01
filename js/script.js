61de952ed9400c2b99232f3bd69b0e82f8aa8962
bajadevmx <91626603+bajadevmx@users.noreply.github.com>
2024-02-21 22:30:24 -0800

    Update

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// // STEP 1
+// let someMonth
+// function theMonth(params) {
+    
+// }
+
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// // STEP 2
+// 700 				// numeric literal 
+// 'SDCCD' 			// string literal 
+// false 				// Boolean literal 
+// null 				// literal null value
+
+// //STEP 3
+// let Total= 20+10
+// let Msj= 'Assignment' + ' Done'
+
+// //STEP 4
+// let strFistName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let intZipCode
+// let intYourAge
+// let strReferralSource
+// let blnMayWeContactYou
+
+// // STEP 5
+// strFistName = "Tania"
+// let strName= "Tania"
+
+// strLastName= "Mendez"
+// let strLastName= "Mendez"
+
+// let strMyName= "Tania Mendez", blnMayWeContactYou=true
+
+// intZipCode= 92154
+// blnMayWeContactYou= true
+
+// //STEP 6
+let MyVariable=  "My number is" + 43
+console.log(MyVariable)
